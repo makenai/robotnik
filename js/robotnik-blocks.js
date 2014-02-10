cd4958fc351212f98506cc4208b6bcc70a89e314
Pawel Szymczykowski <makenai@gmail.com>
2014-02-09 22:03:59 -0800

    Cheating. Because I hate life.

--- js/robotnik-blocks.js
+++ js/robotnik-blocks.js
@@ -2,11 +2,12 @@ Blockly.Blocks['led_on'] = {
   // Numeric value.
   init: function() {
     this.setHelpUrl("http://google.com");
-    this.setColour(230);
+    this.setColour(120);
     this.appendDummyInput()
-        .appendField('LED on');
-    this.setOutput(true, 'Number');
+        .appendField('turn LED on');
     this.setTooltip("Turn the LED on");
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);    
   }
 };
 
@@ -14,11 +15,12 @@ Blockly.Blocks['led_off'] = {
   // Numeric value.
   init: function() {
     this.setHelpUrl("http://google.com");
-    this.setColour(230);
+    this.setColour(360);
     this.appendDummyInput()
-        .appendField('LED off');
-    this.setOutput(true, 'Number');
+        .appendField('turn LED off');
     this.setTooltip("Turn the LED off");
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
   }
 };
 
@@ -35,5 +37,21 @@ Blockly.Blocks['blink_led'] = {
         .appendField('ms.');
     this.setOutput(true, 'Number');
     this.setTooltip("Blink a LED on and off for a specified amount of time");
+  }
+};
+
+Blockly.Blocks['while_button'] = {
+  // Numeric value.
+  init: function() {
+    this.setHelpUrl("http://google.com");
+    this.setColour(180);
+    this.appendDummyInput()
+        .appendField('while')
+        .appendField(new Blockly.FieldDropdown([['red', 'RED'], ['blue', 'BLUE']]))
+        .appendField('button is pressed');
+    this.appendStatementInput('DO')
+      .appendField('do');
+    this.setOutput(true, 'Number');
+    this.setTooltip("Turn the LED off");
   }
 };
