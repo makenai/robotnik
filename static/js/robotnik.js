7b43d2ad0dc2e27c8cc28395f28c6426ee5ab8a4
Pawel Szymczykowski <makenai@gmail.com>
2014-03-02 21:33:45 -0800

    Added some boiler code

--- static/js/robotnik.js
+++ static/js/robotnik.js
@@ -1,8 +1,16 @@
 $( document ).ready(function() {
 
+  var preCode = [ 'var five = require("johnny-five")',
+    'board = new five.Board(),',
+    'led = new five.'
+    'board.on("ready", function() {',
+    '' ];
+
+  var postCode = [ '', '});' ];
+
   $('#execute').on('click', function(e) {
     var code = Blockly.JavaScript.workspaceToCode();
-    console.log( code );
+    console.log( preCode.join("\n") + code + postCode.join("\n") );
   });
 
 });
