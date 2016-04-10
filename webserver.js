02d61261c9fb1c4d9eeca60f0f6948a41ae37168
Pawel Szymczykowski <makenai@gmail.com>
2016-04-10 02:16:56 -0700

    Added support for webserver + websockets version. Fixed race condition with bundled workshop loading.

---
+++ webserver.js
@@ -0,0 +1,29 @@
+var express= require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var CodeRunner = require('./lib/codeRunner');
+
+var codeRunner = new CodeRunner();
+
+app.use(express.static('static'));
+
+io.on('connection', function(socket) {
+
+  socket.on('code', function(code){
+    codeRunner.run( code );
+  });
+
+  socket.on('stop', function(){
+    codeRunner.stop();
+  });
+
+  socket.on('control', function(control){
+    codeRunner.controlEvent( control[0], control[1] );
+  });
+
+});
+
+http.listen(3000, function() {
+  console.log('Listening on http://localhost:3000')
+});
