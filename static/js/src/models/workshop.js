53c329837fb26a04f21744ab7486c90b016fbca2
ajfisher <ajfisher.td@gmail.com>
2016-04-24 15:23:10 +1000

    added getComponents() to workshop model to return comps for an exercise

--- static/js/src/models/workshop.js
+++ static/js/src/models/workshop.js
@@ -1,11 +1,14 @@
 /**
  * A single workshop / smart wrapper for workshop data from the databaase.
  */
+import _ from 'lodash';
 
 export default function Workshop(data) {
   this.data = data;
   if (this.hasExercises()) {
-    this.setExercise(1);
+    this.setExercise(0);
+  } else {
+      throw Error("Workshop has no exercises to run");
   }
 }
 
@@ -19,4 +22,32 @@ Workshop.prototype.setExercise = function( num ) {
 
 Workshop.prototype.hasExercises = function() {
   return this.data.exercises && this.data.exercises.length > 0;
+}
+
+Workshop.prototype.getComponents = function() {
+  // uses the currently selected exercise to generate an array of relevant components
+
+  let workshopComponents = [];
+  if (this.data.components && this.data.components.length > 0) {
+    workshopComponents = this.data.components;
+  }
+
+  let exerciseComponents = [];
+  if (this.data.exercises[this.currentExercise].components &&
+          this.data.exercises[this.currentExercise].components.length > 0) {
+
+    // get the component list then find the ones relevant and push onto the
+    // exercise component list.
+    let componentList = this.data.exercises[this.currentExercise].components;
+    for (let i = 0; i < componentList.length; i++) {
+        exerciseComponents.push(
+            _.find(workshopComponents, { "name": componentList[i] })
+        );
+    }
+  } else {
+      console.warn("No components for this exercise, using all workshop ones instead");
+      exerciseComponents = workshopComponents;
+  }
+
+  return exerciseComponents;
 }
