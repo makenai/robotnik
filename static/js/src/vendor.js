aa2d0051d66dbc5aa83a1b211b22eb9d8df62d95
ajfisher <ajfisher.td@gmail.com>
2016-05-27 23:30:27 +1000

    getting better websockets to angular integration

--- static/js/src/vendor.js
+++ static/js/src/vendor.js
@@ -16,18 +16,3 @@ require('angular-pouchdb');
 
 window.jsBeautify = require('js-beautify').js_beautify;
 
-window.socket.on('consoledata', function(data) {
-    // we use this to write data back to the console for the user
-    // to be able to see.
-    if (data.data != undefined) {
-        var consolestr = data.data;
-
-        if (consolestr.search("Initialized") >= 0) {
-            var title = document.querySelector('h4.modal-title');
-            if (title != undefined) {
-                title.textContent = "Now running";
-            }
-        }
-        console.log(consolestr);
-    }
-});
