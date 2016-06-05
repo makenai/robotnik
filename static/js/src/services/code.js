7561ef41e3febea4cc78866975c168bab83d76aa
ajfisher <ajfisher.td@gmail.com>
2016-06-05 22:13:35 +1000

    Added ability to set board options at a per exercise level and refactor of the way the board code is created

--- static/js/src/services/code.js
+++ static/js/src/services/code.js
@@ -33,12 +33,23 @@ function code(commands, blockly, Boards) {
   function generate() {
 
     // Get board-specific before / after code
-    var boardPre = '', boardPost = '';
+    var boardReqs = '', boardPre = '', boardPost = '', boardInit = '';
     var board = Boards.getForWorkshop( currentWorkshop );
     if (board && board.code) {
+      boardReqs = board.code.requires ? board.code.requires.join("\n") : '';
       boardPre = board.code.before ? board.code.before.join("\n") : '';
       boardPost = board.code.after ? board.code.after.join("\n") : '';
+      boardInit = board.code.boardInit ? board.code.boardInit : '';
     }
+
+    // get the board options for the current workshop
+    var boardopts = currentWorkshop.getBoardConfig();
+    if (boardopts == "") {
+        boardInit = boardInit.replace('opts', "");
+    } else {
+        boardInit = boardInit.replace('opts', JSON.stringify(boardopts));
+    }
+    //boardInit = "var board = new five.Board();"
 
     // Get component initialization code
     var componentCode = getComponentCode( currentWorkshop );
@@ -46,7 +57,9 @@ function code(commands, blockly, Boards) {
     // Get blockly code
     var blocklyCode = blockly.code();
 
-    var code = [ boardPre, componentCode, blocklyCode, boardPost ].join("\n");
+    var code = [ boardReqs, boardInit, boardPre,
+                componentCode, blocklyCode, boardPost
+               ].join("\n");
     return jsBeautify(code, {});
   }
 }
